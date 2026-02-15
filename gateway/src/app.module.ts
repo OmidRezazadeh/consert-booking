61125aaee6c7533b25ec862ba31d6d8a132b865b
orprogrammer <orprogrammer@gmail.com>
2026-02-15 23:24:49 +0330

    add booking and concert modules with controllers and services; implement user module

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { BookingModule } from './booking/booking.module';
+import { ConcertModule } from './concert/concert.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, BookingModule, ConcertModule],
   controllers: [AppController],
   providers: [AppService],
 })
